eba128f883abf7966d7cd7f10f61be49688d42ac
Jiya <coderaholic21@gmail.com>
2024-04-01 23:47:58 +0530

    feat: Added Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React from 'react'
-import Viewer from './components/Viewer'
-import VRrooms from './components/VRrooms'
-import './App.css'
-import C2 from './components/C2'
+import React from "react";
+import {
+  BrowserRouter ,
+  Routes, Route
+} from "react-router-dom";
+import Ai_assistance from "./components/Ai_assistance";
+import Home from "./components/Home"
 
-const App = () => {
+export default function App() {
   return (
-    <div>
-      <div className="ct-1">
-        <Viewer/>
-      </div>
-      {/* <VRrooms/> */}
-      <C2/>
-      {/* <div>
-      <Card border="border-left-behind" shadowUrl="shadow(style='--url: url('https://i.ibb.co/PM4ghD4/full.png')" backgroundUrl="https://i.ibb.co/JpJVJxq/Background.png" cutoutUrl="https://i.ibb.co/Dw3q3tZ/cutout.png" />
-      <Card border='border-right-behind border-bottom-behind' shadowUrl='shadow(style="--url: url("https://i.ibb.co/DC0MbxS/m-full.png")' backgroundUrl='https://i.ibb.co/ZdGBm4K/m-background.png' cutoutUrl='https://i.ibb.co/RC70XmC/m-cutout.png' />
-  
-      <Card border='border-left-behind' shadowUrl="shadow(style='--url: url(\'https://i.ibb.co/gSBp82C/b-full.png\')')" backgroundUrl='https://i.ibb.co/MDBcyMW/b-background.png' cutoutUrl='https://i.ibb.co/bQNgD6y/b-cutout.png' />
-  
-  
-    </div> */}
-
-    </div>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/ai_assistance' element={<Ai_assistance/>} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Viewer from './Viewer'
+// import VRrooms from './components/VRrooms'
+import '../App.css'
+import C2 from './C2'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="ct-1">
+        <Viewer/>
+      </div>
+      {/* <VRrooms/> */}
+      <C2/>
+      {/* <div>
+      <Card border="border-left-behind" shadowUrl="shadow(style='--url: url('https://i.ibb.co/PM4ghD4/full.png')" backgroundUrl="https://i.ibb.co/JpJVJxq/Background.png" cutoutUrl="https://i.ibb.co/Dw3q3tZ/cutout.png" />
+      <Card border='border-right-behind border-bottom-behind' shadowUrl='shadow(style="--url: url("https://i.ibb.co/DC0MbxS/m-full.png")' backgroundUrl='https://i.ibb.co/ZdGBm4K/m-background.png' cutoutUrl='https://i.ibb.co/RC70XmC/m-cutout.png' />
+  
+      <Card border='border-left-behind' shadowUrl="shadow(style='--url: url(\'https://i.ibb.co/gSBp82C/b-full.png\')')" backgroundUrl='https://i.ibb.co/MDBcyMW/b-background.png' cutoutUrl='https://i.ibb.co/bQNgD6y/b-cutout.png' />
+  
+  
+    </div> */}
+
+    </div>
+  )
+}
+
+export default Home
